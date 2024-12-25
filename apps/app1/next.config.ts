097262681fd5edb1525ba856929e03ccf93022ef
Isabelo Nicolas <isabelo.nicolas@gmail.com>
2024-12-25 11:15:10 -0800

    Various improvements
    - Upgrade to Next 15
    - Fix SASS warnings
    - Update various packages

--- apps/app1/next.config.ts
+++ apps/app1/next.config.ts
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
 import dotenv from "dotenv"
 
 dotenv.config({ path: `environments/.env.${process.env.APP_ENV}` })
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
   trailingSlash: true,
-}
+};
 
-export default nextConfig
+export default nextConfig;
